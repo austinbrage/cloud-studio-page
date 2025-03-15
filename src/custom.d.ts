ff50b7c72ff7c4d3d66a09c9becca20cb10e2d6c
Agustin <agustinbrage19@gmail.com>
2025-03-15 17:30:18 -0300

    types: add types and vite config to allow import of video assets

---
+++ src/custom.d.ts
@@ -0,0 +1,14 @@
+declare module '*.mkv' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.webm' {
+    const src: string;
+    export default src;
+}
